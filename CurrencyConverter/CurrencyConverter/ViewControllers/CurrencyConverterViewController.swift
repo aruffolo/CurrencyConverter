58b5fa05711152644091a232401e11fa8342a2ba
Antonio Ruffolo <antonioruffolo2@gmail.com>
2019-08-07 11:15:04 +0200

    Created layout for the main screen. Connected outlets and actions

---
+++ CurrencyConverter/CurrencyConverter/ViewControllers/CurrencyConverterViewController.swift
@@ -0,0 +1,34 @@
+//
+//  CurrencyConverterViewController.swift
+//  CurrencyConverter
+//
+//  Created by Ruffolo Antonio on 07/08/2019.
+//  Copyright © 2019 AR. All rights reserved.
+//
+
+import UIKit
+
+class CurrencyConverterViewController: UIViewController
+{
+    @IBOutlet weak var currencyTopLabel: UILabel!
+    @IBOutlet weak var currencyBottomLabel: UILabel!
+    @IBOutlet weak var enterAmountTopTextField: UITextField!
+    @IBOutlet weak var enterAmountBottomTextField: UITextField!
+    
+    override func viewDidLoad()
+    {
+        super.viewDidLoad()
+    }
+    
+    @IBAction func selectCurrencyTopAction(_ sender: UIButton)
+    {
+    }
+    
+    @IBAction func selectCurrencyBottomAction(_ sender: UIButton)
+    {
+    }
+    
+    @IBAction func convertAction(_ sender: UIButton)
+    {
+    }
+}
