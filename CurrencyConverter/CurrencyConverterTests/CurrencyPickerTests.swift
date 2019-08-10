ae7028ea77fe60a66803ca818e3e18efa56b3934
Antonio Ruffolo <antonioruffolo2@gmail.com>
2019-08-10 02:40:29 +0200

    Added unit test for the view model of the picker

---
+++ CurrencyConverter/CurrencyConverterTests/CurrencyPickerTests.swift
@@ -0,0 +1,44 @@
+//
+//  CurrencyPickerTests.swift
+//  CurrencyConverterTests
+//
+//  Created by Antonio Ruffolo on 10/08/2019.
+//  Copyright © 2019 AR. All rights reserved.
+//
+
+import XCTest
+@testable import CurrencyConverter
+
+class CurrencyPickerTests: XCTestCase
+{
+    override func setUp()
+    {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown()
+    {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testClosePressed()
+    {
+        let viewController = CurrencyPickerViewControllerMock()
+        let pickerViewModel = CurrencyPickerViewModel(view: viewController)
+
+        pickerViewModel.cancelButtonPressed()
+
+        XCTAssert(viewController.closePressed)
+    }
+
+    func testCloseWithIndexPressed()
+    {
+        let viewController = CurrencyPickerViewControllerMock()
+        var pickerViewModel = CurrencyPickerViewModel(view: viewController)
+
+        pickerViewModel.indexChanged(index: 19)
+        pickerViewModel.doneButtonPressed()
+
+        XCTAssert(viewController.indexSelectedArrived == 19)
+    }
+}
---
+++ CurrencyConverter/CurrencyConverterTests/Mock/CurrencyPickerViewControllerMock.swift
@@ -0,0 +1,26 @@
+//
+//  CurrencyPickerViewControllerMock.swift
+//  CurrencyConverterTests
+//
+//  Created by Antonio Ruffolo on 10/08/2019.
+//  Copyright © 2019 AR. All rights reserved.
+//
+
+import UIKit
+@testable import CurrencyConverter
+
+class CurrencyPickerViewControllerMock: UIViewController, CurrencyPickerViewProtocol
+{
+    var closePressed: Bool = false
+    var indexSelectedArrived: Int = 0
+
+    func closeView()
+    {
+        closePressed = true
+    }
+
+    func closeWithIndexSelected(index: Int)
+    {
+        indexSelectedArrived = index
+    }
+}
