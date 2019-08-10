bf1dfc20c91beca5fe0735c62e963f4ba82a9241
Antonio Ruffolo <antonioruffolo2@gmail.com>
2019-08-11 01:46:21 +0200

    Added ui test to check if the 'Amount converted' text field keeps its value after having an error

--- CurrencyConverter/CurrencyConverterUITests/CurrencyConverterUITests.swift
+++ CurrencyConverter/CurrencyConverterUITests/CurrencyConverterUITests.swift
@@ -8,27 +8,33 @@
 
 import XCTest
 
-class CurrencyConverterUITests: XCTestCase {
+class CurrencyConverterUITests: XCTestCase
+{
+    var app: XCUIApplication!
 
-    override func setUp() {
-        // Put setup code here. This method is called before the invocation of each test method in the class.
-
-        // In UI tests it is usually best to stop immediately when a failure occurs.
+    override func setUp()
+    {
         continueAfterFailure = false
 
-        // UI tests must launch the application that they test. Doing this in setup will make sure it happens for each test method.
-        XCUIApplication().launch()
-
-        // In UI tests it’s important to set the initial state - such as interface orientation - required for your tests before they run. The setUp method is a good place to do this.
+        app = XCUIApplication()
+        app.launch()
     }
 
-    override func tearDown() {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
-    }
+    func testAmountConvertedKeepsPlaceholder()
+    {
+        let convertButton = app.buttons["CONVERT ↓"]
+        convertButton.tap()
 
-    func testExample() {
-        // Use recording to get started writing UI tests.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
-    }
+        let closeButton = app.alerts["Error"].buttons["Close"]
+        closeButton.tap()
+        convertButton.tap()
+        closeButton.tap()
 
+        if let textfieldValue = app.textFields["amountConverted"].value as? String
+        {
+            XCTAssertTrue(textfieldValue == "Amount converted")
+        }
+
+        XCTAssertTrue(app.textFields["amountConverted"].placeholderValue == "Amount converted")
+    }
 }
