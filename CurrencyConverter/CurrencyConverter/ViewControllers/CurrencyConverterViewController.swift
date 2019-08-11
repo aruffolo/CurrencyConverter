577041b195dc489d9d97bb62c434391a15c8adf6
Antonio Ruffolo <antonioruffolo2@gmail.com>
2019-08-11 02:44:14 +0200

    changed the layout of the main view.
    Added shadows, corner radius. Reduced some fonts size

--- CurrencyConverter/CurrencyConverter/ViewControllers/CurrencyConverterViewController.swift
+++ CurrencyConverter/CurrencyConverter/ViewControllers/CurrencyConverterViewController.swift
@@ -14,7 +14,10 @@ class CurrencyConverterViewController: UIViewController, CurrencyConverterViewPr
     @IBOutlet weak var currencyBottomLabel: UILabel!
     @IBOutlet weak var enterAmountTopTextField: UITextField!
     @IBOutlet weak var enterAmountBottomTextField: UITextField!
-    
+    @IBOutlet weak var selectCurrencyTop: UIButton!
+    @IBOutlet weak var selectCurrencyBottom: UIButton!
+    @IBOutlet weak var convertButton: UIButton!
+
     var viewModel: CurrencyConverterViewModelProtocol!
     
     var showCurrencyList: ((_ currencies: [String]) -> Void)?
@@ -31,6 +34,40 @@ class CurrencyConverterViewController: UIViewController, CurrencyConverterViewPr
     {
         super.viewWillAppear(animated)
         viewModel.viewWillAppear()
+
+        setStyle()
+    }
+
+    private func setStyle()
+    {
+        setShadows()
+        setCornerRadius()
+
+        convertButton.layer.borderWidth = 2
+        convertButton.layer.borderColor = UIColor.init(red: 5/255, green: 100/255, blue: 255/255, alpha: 1.0).cgColor
+    }
+
+    private func setShadows()
+    {
+        setShadow(layer: selectCurrencyTop.layer)
+        setShadow(layer: selectCurrencyBottom.layer)
+        setShadow(layer: convertButton.layer)
+    }
+
+    private func setCornerRadius()
+    {
+        selectCurrencyTop.layer.cornerRadius = selectCurrencyTop.frame.height / 2
+        selectCurrencyBottom.layer.cornerRadius = selectCurrencyTop.frame.height / 2
+
+        convertButton.layer.cornerRadius = convertButton.frame.height / 2
+    }
+
+    private func setShadow(layer: CALayer)
+    {
+        layer.shadowColor = UIColor.black.cgColor
+        layer.shadowOpacity = 0.4
+        layer.shadowRadius = 4.0
+        layer.shadowOffset = CGSize(width: 1.0, height: 1.0)
     }
     
     // this should call the view model
@@ -102,9 +139,9 @@ class CurrencyConverterViewController: UIViewController, CurrencyConverterViewPr
 
 extension CurrencyConverterViewController: UITextFieldDelegate
 {
-    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool
+    {
         textField.resignFirstResponder()
-        
         viewModel.topAmountChanged(amount: textField.text ?? "")
 
         return true
